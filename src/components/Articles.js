6f49c7aca200043ea65e46e6c0b1475c20d89544
serhat-ozcakir <serhatcakir87@gmail.com>
2022-02-16 23:03:47 +0300

    new comit

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -24,14 +24,14 @@ function Articles({ upvote, articles, date }) {
               ))
             : date
             ? date.map((item) => (
-                <tr data-testid="article" key="article-index1">
+                <tr data-testid="article" key="article-index">
                   <td data-testid="article-title">{item.title}</td>
                   <td data-testid="article-upvotes">{item.upvotes}</td>
                   <td data-testid="article-date">{item.date}</td>
                 </tr>
               ))
             : articles.map((item) => (
-                <tr data-testid="article" key="article-index2">
+                <tr data-testid="article" key="article-index">
                   <td data-testid="article-title">{item.title}</td>
                   <td data-testid="article-upvotes">{item.upvotes}</td>
                   <td data-testid="article-date">{item.date}</td>
