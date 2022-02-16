688038c6adf45af18b280e29c62c9071d72b0e62
serhat-ozcakir <serhatcakir87@gmail.com>
2022-02-16 21:52:51 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
-
-import Articles from './components/Articles';
+import React, { useState } from "react";
+import "./App.css";
+import "h8k-components";
+import Articles from "./components/Articles";
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+  const [upvote, setUpvote] = useState();
+  const [date, setDate] = useState()
+  console.log(upvote);
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
 
+
+  const onUpvote = () => {
+    setDate()
+    articles.sort((a,b) => (a.upvotes > b.upvotes) ? -1 : ((b.upvotes > a.upvotes) ? 1 : 0))
+    setUpvote(articles)
+
+      console.log(upvote);
+    };
+
+ 
+    
+    const onRecent = () => {
+        setUpvote()
+        articles.sort((a,b) => (a.date > b.date) ? -1 : ((b.date > a.date) ? 1 : 0))
+        setDate(articles)
+    
+  };
+
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          data-testid="most-upvoted-link"
+          className="small"
+          onClick={onUpvote}
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid="most-recent-link"
+          className="small"
+          onClick={onRecent}
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles upvote={upvote} articles={articles} date={date} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React from "react";
 
-function Articles() {
+function Articles({ upvote, articles, date }) {
+  // console.log(upvote,articles);
 
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {upvote
+            ? upvote.map((item) => (
                 <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
+                  <td data-testid="article-title">{item.title}</td>
+                  <td data-testid="article-upvotes">{item.upvotes}</td>
+                  <td data-testid="article-date">{item.date}</td>
                 </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+              ))
+            : date
+            ? date.map((item) => (
+                <tr data-testid="article" key="article-index">
+                  <td data-testid="article-title">{item.title}</td>
+                  <td data-testid="article-upvotes">{item.upvotes}</td>
+                  <td data-testid="article-date">{item.date}</td>
+                </tr>
+              ))
+            : articles.map((item) => (
+                <tr data-testid="article" key="article-index">
+                  <td data-testid="article-title">{item.title}</td>
+                  <td data-testid="article-upvotes">{item.upvotes}</td>
+                  <td data-testid="article-date">{item.date}</td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
